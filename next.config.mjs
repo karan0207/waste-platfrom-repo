3be2ecebec7fa3ec5631977d40750d78cc347a30
Karan0207 <minidox0207@gmail.com>
2025-04-21 19:52:41 +0530

    fixes:cors

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,24 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'credentialless',
+          }
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
